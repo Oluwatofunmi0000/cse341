015dd8ce9668b54f1f76a091c6ec67ab2bf2332a
Oluwatofunmi0000 <oyalekeoluwatofunmi@gmail.com>
2025-11-13 04:03:21 +0100

    ...

--- server.js
+++
@@ -1,8 +0,0 @@
-// express web server
-const express = require('express');
-  const app = express();
-
-  app.get('/', (req, res) => {res.send(' Rose Oyaleke');});
- const port = 3000;
- app.listen(process.env.port || 3000);
- console.log('web server is listening at port' + (process.env.port || 3000));
