256133fd4029a20ddc0aad985ba6d2317b15aefc
Oluwatofunmi0000 <oyalekeoluwatofunmi@gmail.com>
2025-11-13 02:43:04 +0100

    ..

--- contacts-api/contacts/contactModel.js
+++ contacts-api/contacts/contactModel.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');..
+const { MongoClient } = require('mongodb');
 
 const uri = 'mongodb://localhost:27017'; // Update with your connection string
 const client = new MongoClient(uri);
